3cc09f81cc1311c672d879d24e98587d5a6954eb
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-06-19 16:20:51 -0700

    t

--- src/app/fillPDF/page.tsx
+++ src/app/fillPDF/page.tsx
@@ -10,51 +10,51 @@ export default async function page() {
   const proto = headersList.get("X-Forwarded-Proto");
 
   const url = "http://" + host + "/formFillPDF.pdf";
-  //const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
-  //   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+//   const url = "https://pdf-lib.js.org/assets/with_update_sections.pdf";
+  const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
 
-  //   const pdfDoc = await PDFDocument.load(existingPdfBytes);
-  //   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-  //   const form = pdfDoc.getForm();
+  const form = pdfDoc.getForm();
 
-  //   const nameField = form.getTextField("Text1");
-  //   nameField.setText(
-  //     [
-  //       `Mario is a fictional character in the Mario video game franchise, `,
-  //       `owned by Nintendo and created by Japanese video game designer Shigeru `,
-  //       `Miyamoto. Serving as the company's mascot and the eponymous `,
-  //       `protagonist of the series, Mario has appeared in over 200 video games `,
-  //       `since his creation. Depicted as a short, pudgy, Italian plumber who `,
-  //       `resides in the Mushroom Kingdom, his adventures generally center `,
-  //       `upon rescuing Princess Peach from the Koopa villain Bowser. His `,
-  //       `younger brother and sidekick is Luigi.`,
-  //     ].join("\n")
-  //   );
+  const nameField = form.getTextField("Text1");
+  nameField.setText(
+    [
+      `Mario is a fictional character in the Mario video game franchise, `,
+      `owned by Nintendo and created by Japanese video game designer Shigeru `,
+      `Miyamoto. Serving as the company's mascot and the eponymous `,
+      `protagonist of the series, Mario has appeared in over 200 video games `,
+      `since his creation. Depicted as a short, pudgy, Italian plumber who `,
+      `resides in the Mushroom Kingdom, his adventures generally center `,
+      `upon rescuing Princess Peach from the Koopa villain Bowser. His `,
+      `younger brother and sidekick is Luigi.`,
+    ].join("\n")
+  );
 
-  //   form.flatten();
+  form.flatten();
 
-  //   const pages = pdfDoc.getPages();
-  //   const firstPage = pages[0];
-  //   const { width, height } = firstPage.getSize();
-  //   firstPage.drawText("This text was added with JavaScript!", {
-  //     x: 5,
-  //     y: height / 2 + 300,
-  //     size: 20,
-  //     font: helveticaFont,
-  //     color: rgb(0.95, 0.1, 0.1),
-  //     //rotate: degrees(-45),
-  //   });
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+  const { width, height } = firstPage.getSize();
+  firstPage.drawText("This text was added with JavaScript!", {
+    x: 5,
+    y: height / 2 + 300,
+    size: 20,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    //rotate: degrees(-45),
+  });
 
-  //   const pdfBytes = await pdfDoc.save();
+  const pdfBytes = await pdfDoc.save();
 
-  //   fs.writeFile("public/filledPDF.pdf", pdfBytes, (err) => {
-  //     if (err) {
-  //       console.error(err);
-  //     } else {
-  //       // file written successfully
-  //     }
-  //   });
+  fs.writeFile("public/filledPDF.pdf", pdfBytes, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // file written successfully
+    }
+  });
 
   const files = fs.readdirSync(path.join("public/"));
 
